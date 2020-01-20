245e561e3bdbd355a6094acb9adc511d68a5d1eb
Hareesh Nalagatla <HNalagatla@alphaserveit.com>
2020-01-20 14:22:18 +0530

    commit test folder

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var names = [1, 2, 3, 4, 5, 6];
+var name1 = "hareesh";
+
+names.forEach(function(val) {
+    console.log(val);
+});
+
+console.log(name1);
+
---
+++ test/testsample.js
@@ -0,0 +1,4 @@
+var name = 'hareesh'
+var name1 = 'hareesh2'
+var name;
+var name1 = 'hareesh3';
